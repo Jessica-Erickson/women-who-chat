8544fe913be113297cbaec44f3ad0aba29fab631
Intrepid Panda <jerick999@gmail.com>
2019-02-04 13:55:50 -0700

    Make working chat app

---
+++ public/index.js
@@ -0,0 +1,23 @@
+// const url = 'wss://women-who-chat.herokuapp.com';
+const url = 'ws://localhost:8080';
+const connection = new WebSocket(url);
+
+connection.onmessage = e => {
+  appendMessage(e.data);
+}
+
+const appendMessage = (message) => {
+  const messageList = document.querySelector('ul');
+  messageList.innerHTML += `<li>${message}</li>`;
+  messageList.scrollTop = messageList.scrollHeight;
+}
+
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const input = document.querySelector('#message');
+
+  if (input.value.length > 0) {
+    connection.send(input.value);
+    input.value = '';
+  }
+});
---
+++ server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const app = express();
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  ws.userNum = 0;
+
+  while (app.locals.defaultUsers[ws.userNum].inUse) {
+    if (ws.userNum < 19) {
+      ws.userNum += 1;
+    } else {
+      return ws.send('This chat room is full.');
+    }
+  }
+
+  app.locals.defaultUsers[ws.userNum].inUse = true;
+
+  app.locals.messages.forEach(message => {
+    ws.send(message);
+  });
+
+  app.locals.messages.push(`A ${app.locals.defaultUsers[ws.userNum].name} has joined the chat.`);
+
+  wss.broadcast(`A ${app.locals.defaultUsers[ws.userNum].name} has joined the chat.`);
+
+
+  ws.on('message', (message) => {
+    app.locals.messages.push(`${app.locals.defaultUsers[ws.userNum].name} says: ${message}`);
+
+    wss.broadcast(`${app.locals.defaultUsers[ws.userNum].name} says: ${message}`);
+  });
+
+  ws.on('close', () => {
+    app.locals.defaultUsers[ws.userNum].inUse = false;
+
+    wss.broadcast(`A ${app.locals.defaultUsers[ws.userNum].name} has left the chat.`);
+
+    app.locals.messages.push(`A ${app.locals.defaultUsers[ws.userNum].name} has left the chat.`);
+  });
+});
+
+wss.broadcast = (data) => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(data);
+    }
+  });
+};
+
+app.locals.messages = [];
+app.locals.defaultUsers = [
+  {name: 'Red Panda', inUse: false},
+  {name: 'Ocelot', inUse: false},
+  {name: 'Kangaroo', inUse: false},
+  {name: 'Walrus', inUse: false},
+  {name: 'Hedgehog', inUse: false},
+  {name: 'Cockatoo', inUse: false},
+  {name: 'Giraffe', inUse: false},
+  {name: 'Aligator', inUse: false},
+  {name: 'Otter', inUse: false},
+  {name: 'Elephant', inUse: false},
+  {name: 'Axolotl', inUse: false},
+  {name: 'Cheetah', inUse: false},
+  {name: 'Platypus', inUse: false},
+  {name: 'Lemur', inUse: false},
+  {name: 'Finch', inUse: false},
+  {name: 'Kakapo', inUse: false},
+  {name: 'Python', inUse: false},
+  {name: 'Komodo Dragon', inUse: false},
+  {name: 'Chicken', inUse: false},
+  {name: 'Moose', inUse: false}
+];
+
+app.use(express.static('public'));
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+  console.log(`women who chat is running on port ${app.get('port')}`);
+});
